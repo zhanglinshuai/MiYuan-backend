28ec3303e73449bc542e7196480ad7a890c7fbba
zls <1216486490@qq.com>
2024-04-16 23:29:28 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# MiYuan-backend
---
+++ src/main/java/com/example/MiYuanBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiYuanBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MiYuanBackendApplication.class, args);
+    }
+
+}
